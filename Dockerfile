6c92f0d49fed00d7b58437bb3fad8a57995b7e62
Osvald Lindholm <osvald.lindholm@gmail.com>
2022-09-26 19:24:44 +0200

    Cleanup and modernize python structure

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3
-WORKDIR /app
-
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
--- src/conftest.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# This file does nothing but is important!?
-# This file ensures pytest finds all the
-# modules here in src.
-# https://stackoverflow.com/q/20971619
-# https://stackoverflow.com/a/34520971/4255176
